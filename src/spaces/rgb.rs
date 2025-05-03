81bddf52d88da06653f8735ac6e95ea1bba03810
OwOchlé <romeoboub@gmail.com>
2025-05-03 14:14:33 +0200

    fix: index out of range with rgb

--- src/spaces/rgb.rs
+++ src/spaces/rgb.rs
@@ -5,7 +5,7 @@ pub fn rgb_space(colors: &Vec<Hsv>, factor: f32) -> Srgb<u8> {
     let index = factor.floor() as usize;
 
     let a: Rgb = colors[index % colors.len()].into_color();
-    let b: Rgb = colors[index + 1 % colors.len()].into_color();
+    let b: Rgb = colors[(index + 1) % colors.len()].into_color();
 
     a.mix(b, factor % 1.0).into_format()
 }
